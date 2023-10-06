e11869f568438bb65efba7d08ff1206e019a2508
Seth Fogarty <sfogarty@trinity.edu>
2023-10-06 12:22:22 -0500

    push

--- lec20_trees.hs
+++ lec20_trees.hs
@@ -5,13 +5,25 @@ inputTwo   = "- + 79 4 * 8 2"
 inputThree = "* + 3 - 79 8 2"
 
 data Operator = Plus | Minus | Divide | Times deriving (Show, Eq)
-data Token = Op Operator | Num Double deriving (Show, Eq)
+data Token = OpTok Operator | NumTok Double deriving (Show, Eq)
+data Expr = OpExpr Operator Expr Expr | NumExpr Double
 
 --data Token = Plus | Minus | Divide | Times | Num Double deriving (Show, Eq)
+--valid but we voted against it
 
 lexer :: String -> [Token]
 lexer str = map lexWord (words str)
-  where lexWord :: String -> Token
 
---lexWord "7" = Num 7
---lexWord "+" = Op Plus
+lexWord :: String -> Token
+lexWord "+" = OpTok Plus
+lexWord "-" = OpTok Minus
+lexWord "*" = OpTok Times
+lexWord "/" = OpTok Divide
+lexWord x = NumTok (read x)
+--etc
+
+tokensOne = [OpTok Plus,NumTok 7.0,OpTok Times,NumTok 3.0,NumTok 2.0]
+tokensTwo = [OpTok Minus,OpTok Plus,NumTok 79.0,NumTok 4.0,OpTok Times,NumTok 8.0,NumTok 2.0]
+tokensThree = [OpTok Times,OpTok Plus,NumTok 3.0,OpTok Minus,NumTok 79.0,NumTok 8.0,NumTok 2.0]
+
+
