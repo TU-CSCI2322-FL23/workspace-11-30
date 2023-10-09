f6d353019f17af75a72e66123d9ae3bf37b63015
Seth Fogarty <sfogarty@trinity.edu>
2023-10-09 12:22:51 -0500

    push

--- calculator.hs
+++ calculator.hs
@@ -6,7 +6,6 @@ inputThree = "* + 3 - 79 8 2"
 
 data Operator = Plus | Minus | Divide | Times deriving (Show, Eq)
 data Token = OpTok Operator | NumTok Double deriving (Show, Eq)
-data Expr = OpExpr Operator Expr Expr | NumExpr Double
 
 --data Token = Plus | Minus | Divide | Times | Num Double deriving (Show, Eq)
 --valid but we voted against it
@@ -20,12 +19,16 @@ lexWord "-" = OpTok Minus
 lexWord "*" = OpTok Times
 lexWord "/" = OpTok Divide
 lexWord x = NumTok (read x)
---etc
 
+tokensOne, tokensTwo, tokensThree :: [Token]
 tokensOne = [OpTok Plus,NumTok 7.0,OpTok Times,NumTok 3.0,NumTok 2.0]
 tokensTwo = [OpTok Minus,OpTok Plus,NumTok 79.0,NumTok 4.0,OpTok Times,NumTok 8.0,NumTok 2.0]
 tokensThree = [OpTok Times,OpTok Plus,NumTok 3.0,OpTok Minus,NumTok 79.0,NumTok 8.0,NumTok 2.0]
 
+--data Operator = Plus | Minus | Divide | Times deriving (Show, Eq)
+data Expr = OpExpr Operator Expr Expr | NumExpr Double deriving Show
+
+treeZero, treeOne, treeTwo, treeTwoA, treeTwoB, treeThree :: Expr
 treeZero = NumExpr 10.0
 treeOne = OpExpr Plus (NumExpr 7.0) (OpExpr Times (NumExpr 3) (NumExpr 2))
 treeTwo = OpExpr Minus treeTwoA treeTwoB 
@@ -40,3 +43,25 @@ treeThree = OpExpr Times (OpExpr Plus (NumExpr 3.0)
 
 
 eval :: Expr -> Double
+eval (NumExpr x) = x
+eval (OpExpr Plus lft rgt) = (eval lft) + (eval rgt)
+eval (OpExpr Minus lft rgt) = (eval lft) - (eval rgt)
+eval (OpExpr Divide lft rgt) = (eval lft) / (eval rgt)
+eval (OpExpr Times lft rgt) = (eval lft) * (eval rgt)
+
+eval (OpExpr op left right) = 
+  let leftV = eval lft
+      rightV = eval rgt
+  in case op of
+        Plus -> leftV + rightV
+        Minus -> leftV - rightV
+        Divide -> leftV / rightV
+        Times -> leftV * rightV
+
+eval (OpExpr op lft rgt) = (evalOp op) (eval lft) (eval rgt)
+
+evalOp :: Operator -> (Double -> Double -> Double)
+evalOp Plus = (+) 
+evalOp Minus = (-)
+evalOp Times = (*)
+evalOp Divide = (/)
