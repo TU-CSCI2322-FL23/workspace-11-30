293e6194f16f119fee0b6d7a59b45a8426dd6505
Seth Fogarty <sfogarty@trinity.edu>
2023-11-13 12:23:27 -0600

    push

--- Fortunes.hs
+++ Fortunes.hs
@@ -6,33 +6,39 @@ import System.Environment
 import System.IO
 
 main :: IO ()
-main = do
-  args <- getArgs
-  {-let fname = case args of
-                [] -> "fortunes.txt"
-                (x:xs) -> x-}
-  let fname = if null args then "fortunes.txt" else head args
-  name <- prompt "Please tell me your name: "
-  contents <- (readFile fname)
-  let fortunes = lines contents
-  putStr $ "Hello, " ++ name ++ ", your fortune is: "
-  putStrLn $ pickFortune fortunes name
-  resp <- prompt "Do you want another fortune?"
-  if (map toLower resp) `elem` ["y", "yes", "sure", "okay"]
-  then do 
-    putStr $ "Your fortune is: "
-    putStrLn $ pickFortune fortunes (name++"1")
-  else return ()
+main = 
+  do args <- getArgs
+     {-let fname = case args of
+                   [] -> "fortunes.txt"
+                   (x:xs) -> x-}
+     let fname = if null args then "fortunes.txt" else head args
+     if "--help" `elem` args
+     then putStrLn "Fortunes <filename>\n Interactive fortune teller!"
+     else 
+      do contents <- (readFile fname)
+         name <- prompt "Please tell me your name:"
+         let fortunes = lines contents
+         tellFortunes name fortunes
+
+tellFortunes :: String -> [String] -> IO ()
+tellFortunes name fortunes = aux (indexOfName name)
+  where aux index = 
+          do putStr $ "Hello, " ++ name ++ ", your fortune is: "
+             putStrLn $ pickFortune fortunes index
+             resp <- prompt "Do you want another fortune?"
+             if (map toLower resp) `elem` ["y", "yes", "sure", "okay"]
+             then aux (index+1)
+             else return ()
 
 prompt :: String -> IO String
 prompt question = 
-  do putStr question
+  do putStr (question++" ")
      hFlush stdout
      resp <- getLine
      return resp
 
+indexOfName :: String -> Int
+indexOfName name = sum $ map ord name
 
-pickFortune :: [String] -> String -> String
-pickFortune fortunes name =
-  let index = sum $ map ord name
-  in (cycle fortunes) !! index
+pickFortune :: [String] -> Int -> String
+pickFortune fortunes index = (cycle fortunes) !! index
